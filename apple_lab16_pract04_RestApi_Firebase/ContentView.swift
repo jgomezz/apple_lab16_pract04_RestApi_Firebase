277ce79f94893475cc4bdbf07adb0a4a426d9a6e
jgomezz <jgomezz@gmail.com>
2025-06-29 22:41:58 -0500

    Initial Commit

---
+++ apple_lab16_pract04_RestApi_Firebase/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  apple_lab16_pract04_RestApi_Firebase
+//
+//  Created by Jaime Gomez on 29/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ apple_lab16_pract04_RestApi_Firebase/apple_lab16_pract04_RestApi_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_lab16_pract04_RestApi_FirebaseApp.swift
+//  apple_lab16_pract04_RestApi_Firebase
+//
+//  Created by Jaime Gomez on 29/6/25.
+//
+
+import SwiftUI
+
+@main
+struct apple_lab16_pract04_RestApi_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
